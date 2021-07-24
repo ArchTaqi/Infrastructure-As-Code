b5b1a21cccfe5741bacf7f8c677a55c152d8f26c
archtaqi <taqi.arch@gmail.com>
2021-07-25 04:55:09 +0500

    Added Routing tables

--- src/prod/main.tf
+++ src/prod/main.tf
@@ -18,3 +18,11 @@ module "internet_gateway" {
 
   vpc_id = module.vpc.vpc_id
 }
+
+module "route_table" {
+  source = "../modules/route-table"
+
+  vpc_id              = module.vpc.vpc_id
+  internet_gateway_id = module.internet_gateway.internet_gateway_id
+  public_subnet_id    = module.public_subnet.public_subnet_id
+}
