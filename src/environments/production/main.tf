eff66f3c2da1e5f3f055dccfb2299b95dac0a5bf
archtaqi <taqi.arch@gmail.com>
2021-07-25 05:42:27 +0500

    Renaming directories

--- src/environments/production/main.tf
+++ src/environments/production/main.tf
@@ -4,23 +4,23 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "../modules/vpc"
+  source = "../../modules/vpc"
 }
 
 module "public_subnet" {
-  source = "../modules/public-subnet"
+  source = "../../modules/public-subnet"
   vpc_id = module.vpc.vpc_id
   subnet_availability_zone = var.subnet_availability_zone
 }
 
 module "internet_gateway" {
-  source = "../modules/internet-gateway"
+  source = "../../modules/internet-gateway"
 
   vpc_id = module.vpc.vpc_id
 }
 
 module "route_table" {
-  source = "../modules/route-table"
+  source = "../../modules/route-table"
 
   vpc_id              = module.vpc.vpc_id
   internet_gateway_id = module.internet_gateway.internet_gateway_id
@@ -28,7 +28,7 @@ module "route_table" {
 }
 
 module "ec2" {
-  source = "../modules/ec2"
+  source = "../../modules/ec2"
   
   ec2_ami                 = var.ec2_ami
   ec2_instance_type       = var.ec2_instance_type
