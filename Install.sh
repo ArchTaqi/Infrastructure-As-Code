ab12eb06ff4edf9a94761b2e3e0299e39d1dc311
Muhαmmαd Tαqi <mut@ciklum.com>
2021-09-12 16:52:15 +0500

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,17 @@
+
+
+###########################
+## Terragrunt Install Linux
+###########################
+
+wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.31.10/terragrunt_linux_amd64
+mv terragrunt_linux_amd64 terragrunt
+sudo chmod +x terragrunt
+sudo mv terragrunt /usr/local/bin
+
+
+
+
+
+
+
